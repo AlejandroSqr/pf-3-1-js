b1604e9b34270573437e8901255df08e094b74d1
AlejandroDel <alejandrohdzd@proton.me>
2025-08-26 10:51:00 -0600

    Update main.js
    Calculadora Sencilla

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 
 export function addTwoNumbers(/*aqui tambien va código*/){
-  //aqui va tu código
+//Calculadora JS en Consola
+
+const operator = prompt('Ingrese Operación ( +, -, * / ): ');
+
+//Etiqueta ParseFloat en Calculadora
+const n1 = parseFloat(prompt('Num 1:  '));
+const n2 = parseFloat(prompt('Num 2: '));
+
+let result;
+
+if (operator == '+') {
+    result = n1 + n2;
+}
+else if (operator == '-') {
+    result = n1 - n2;
+}
+else if (operator == '*') {
+    result = n1 * n2;
+}
+else {
+    result = n1 / n2;
+}
+
+console.log(`${n1} ${operator} ${n2} = ${result}`);
 }
